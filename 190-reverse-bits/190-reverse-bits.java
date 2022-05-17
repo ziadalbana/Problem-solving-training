96d60e1598e0b6b83fa3181397bd9fd1ea306638
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-05-18 01:59:03 +0200

    Time: 1 ms (89.41%), Space: 41.5 MB (89.81%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -7,7 +7,7 @@ public class Solution {
     for (int i = 0; i < 32; i++) {
         result <<= 1;
         if ((n & 1) == 1) result++;
-        n >>= 1;
+        n =n>> 1;
     }
     return result;
 }
