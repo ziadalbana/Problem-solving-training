0586de5a31091f2971be9b250486ee285ac2dfcf
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-05-17 19:25:03 +0200

    Time: 3 ms (28.57%), Space: 55.2 MB (20.07%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,21 +1,34 @@
 class Solution {
-  public void moveZeroes(int[] nums) {
-      if(nums.length==1) return;
-      int endPointer=nums.length-1;
-      int i=0;
-      while (i <= endPointer){
-             if (nums[i]==0){
-                 shift(nums,i,endPointer);
-                 endPointer--;
-             }else {
-                 i++;
-             }
+     public void moveZeroes(int[] nums) {
+        int snowBallSize = 0; 
+        for (int i=0;i<nums.length;i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
+            }
+            else if (snowBallSize > 0) {
+	            int t = nums[i];
+	            nums[i]=0;
+	            nums[i-snowBallSize]=t;
+            }
         }
     }
-    private void shift(int[] nums,int i,int j){
-        for (int k = i; k <j; k++) {
-            nums[k]=nums[k+1];
-        }
-        nums[j]=0;
-    }
+  // public void moveZeroes(int[] nums) {
+  //     if(nums.length==1) return;
+  //     int endPointer=nums.length-1;
+  //     int i=0;
+  //     while (i <= endPointer){
+  //            if (nums[i]==0){
+  //                shift(nums,i,endPointer);
+  //                endPointer--;
+  //            }else {
+  //                i++;
+  //            }
+  //       }
+  //   }
+  //   private void shift(int[] nums,int i,int j){
+  //       for (int k = i; k <j; k++) {
+  //           nums[k]=nums[k+1];
+  //       }
+  //       nums[j]=0;
+  //   }
 }
