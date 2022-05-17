dad6ca661ba9a68ad09180ccadef32938a287b23
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-05-18 01:27:29 +0200

    Time: 0 ms (100.00%), Space: 42.1 MB (11.34%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,13 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
-    if (nums.length == 0) return 0;
-    int[] memo = new int[nums.length + 1];
-    memo[0] = 0;
-    memo[1] = nums[0];
-    for (int i = 1; i < nums.length; i++) {
-        int val = nums[i];
-        memo[i+1] = Math.max(memo[i], memo[i-1] + val);
+       int size=nums.length;
+       if(size==1) return nums[0];
+       int[] mem=new int[size+1];
+        mem[0]=0;
+        mem[1]=nums[0];
+        for(int i=1;i<size;i++){
+            mem[i+1]=Integer.max(mem[i],mem[i-1]+nums[i]);
+        }
+        return mem[size];
     }
-    return memo[nums.length];
-}
+        
 }
