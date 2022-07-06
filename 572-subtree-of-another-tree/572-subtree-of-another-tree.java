e2a1970cf42f698cfe4c7c9f8150f7fb3cdc3c19
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-07-06 15:26:29 +0200

    Time: 3 ms (97.02%), Space: 46.6 MB (49.13%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -13,33 +13,31 @@
  *     }
  * }
  */
-// class Solution {
-//     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-//         if(root==null) return false;
-//         if(isSame(root,subRoot)) return true;
-//         return isSame(root.left,subRoot)||isSame(root.right,subRoot);
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root==null) return false;
+        return isSame(root,subRoot)||isSubtree(root.left,subRoot)||isSubtree(root.right,subRoot);
         
-//     }
-//     private boolean isSame(TreeNode root, TreeNode subRoot){
-//         if(root==null&&subRoot==null) return true;
-//         if(root==null||subRoot==null) return false;
-//         if(root.val!=subRoot.val) return  false;
-//         return isSame(root.left,subRoot.left)&&isSame(root.right,subRoot.right);
-//     }
-// }
-public class Solution {
-    public boolean isSubtree(TreeNode s, TreeNode t) {
-        if (s == null) return false;
-        if (isSame(s, t)) return true;
-        return isSubtree(s.left, t) || isSubtree(s.right, t);
     }
-    
-    private boolean isSame(TreeNode s, TreeNode t) {
-        if (s == null && t == null) return true;
-        if (s == null || t == null) return false;
-        
-        if (s.val != t.val) return false;
-        
-        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    private boolean isSame(TreeNode root, TreeNode subRoot){
+        if(root==null||subRoot==null) return root==subRoot;
+        if(root.val!=subRoot.val) return  false;
+        return isSame(root.left,subRoot.left)&&isSame(root.right,subRoot.right);
     }
 }
+// public class Solution {
+//     public boolean isSubtree(TreeNode s, TreeNode t) {
+//         if (s == null) return false;
+//         if (isSame(s, t)) return true;
+//         return isSubtree(s.left, t) || isSubtree(s.right, t);
+//     }
+    
+//     private boolean isSame(TreeNode s, TreeNode t) {
+//         if (s == null && t == null) return true;
+//         if (s == null || t == null) return false;
+        
+//         if (s.val != t.val) return false;
+        
+//         return isSame(s.left, t.left) && isSame(s.right, t.right);
+//     }
+// }
