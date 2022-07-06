5a04e550851b1778b56a74748651b18385af1a2b
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-07-06 15:22:32 +0200

    Time: 4 ms (58.33%), Space: 45.9 MB (78.56%) - LeetHub

---
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+//         if(root==null) return false;
+//         if(isSame(root,subRoot)) return true;
+//         return isSame(root.left,subRoot)||isSame(root.right,subRoot);
+        
+//     }
+//     private boolean isSame(TreeNode root, TreeNode subRoot){
+//         if(root==null&&subRoot==null) return true;
+//         if(root==null||subRoot==null) return false;
+//         if(root.val!=subRoot.val) return  false;
+//         return isSame(root.left,subRoot.left)&&isSame(root.right,subRoot.right);
+//     }
+// }
+public class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) return false;
+        if (isSame(s, t)) return true;
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+    
+    private boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+        
+        if (s.val != t.val) return false;
+        
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
+}
