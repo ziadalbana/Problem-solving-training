fe644c73816d62426bea9f6f1966ce864681b00f
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-07-17 13:06:16 +0200

    Time: 15 ms (46.89%), Space: 69.2 MB (53.30%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -3,13 +3,10 @@ class Solution {
         HashMap<Integer,Integer> occurance=new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if(occurance.containsKey(nums[i])){
-                occurance.replace(nums[i], occurance.get(nums[i])+1);
+                return true;
             }else {
                 occurance.put(nums[i],1);
             }  
-        }
-        for (Integer n: occurance.values()) {
-            if(n>1) return true;
         }
         return false;
     }
