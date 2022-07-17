e055ef9af7a24c354d9fd0915a6f717e09c1c194
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-07-17 13:04:50 +0200

    Time: 66 ms (5.01%), Space: 76.8 MB (5.10%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> occurance=new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(occurance.containsKey(nums[i])){
+                occurance.replace(nums[i], occurance.get(nums[i])+1);
+            }else {
+                occurance.put(nums[i],1);
+            }  
+        }
+        for (Integer n: occurance.values()) {
+            if(n>1) return true;
+        }
+        return false;
+    }
+}
