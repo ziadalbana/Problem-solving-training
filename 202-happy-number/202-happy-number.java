687b0a614ba2606db60174e872d2a93eca54fede
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-08-14 13:33:14 +0200

    Time: 1 ms (98.92%), Space: 41.1 MB (61.75%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,12 +1,18 @@
 class Solution {
     public boolean isHappy(int num) {
     if(num==1) return true;
-    HashSet<Integer> set=new HashSet<>();
-    while(num!=1){
-      num =getNum(num);
-      if(!set.add(num)) return false;
-    }
-    return true;
+    // HashSet<Integer> set=new HashSet<>();
+    // while(num!=1){
+    //   num =getNum(num);
+    //   if(!set.add(num)) return false;
+    // }
+    // return true;
+    int slow=num,fast=num;
+    do{
+       slow=getNum(slow);
+       fast=getNum(getNum(fast));
+    }while(slow!=fast);
+    return slow==1;    
   }
   private int getNum(int num){
     int sum=0,temp=0;;
