b766e8abfc532368f2a1caf6f140a90c917f7994
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-08-14 18:37:28 +0200

    Time: 6 ms (78.40%), Space: 97.8 MB (50.00%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -9,25 +9,52 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-    if(head==null) return false;
-    ListNode slow=head;
-    ListNode fast=head;
-    while(fast!=null&&fast.next!=null){
-      fast=fast.next.next;
-      slow=slow.next;
+    // public boolean isPalindrome(ListNode head) {
+    // if(head==null) return false;
+    // ListNode slow=head;
+    // ListNode fast=head;
+    // while(fast!=null&&fast.next!=null){
+    //   fast=fast.next.next;
+    //   slow=slow.next;
+    // }
+    // Stack<Integer> stack=new Stack<>();
+    // ListNode middle=slow;
+    // while(slow!=null){
+    //    stack.add(slow.val);
+    //    slow=slow.next;
+    // }
+    // slow=head;
+    // while(slow!=middle){
+    //   if(slow.val!=stack.pop()) return false;
+    //   slow=slow.next;
+    // }
+    // return true;
+    // }
+     public boolean isPalindrome(ListNode head) {
+        if(head==null) return false;
+        ListNode slow=head;
+        ListNode fast=head;
+        while (fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        ListNode secondHalf=reverse(slow);
+        ListNode copySecondHalf=secondHalf;
+        while (head!=null&&secondHalf!=null){
+            if(head.val!=secondHalf.val) return false;
+            head=head.next;
+            secondHalf=secondHalf.next;
+        }
+      return true;
     }
-    Stack<Integer> stack=new Stack<>();
-    ListNode middle=slow;
-    while(slow!=null){
-       stack.add(slow.val);
-       slow=slow.next;
-    }
-    slow=head;
-    while(slow!=middle){
-      if(slow.val!=stack.pop()) return false;
-      slow=slow.next;
-    }
-    return true;
+    private ListNode reverse(ListNode head){
+        ListNode prev=null;
+        while (head!=null){
+            ListNode next=head.next;
+            head.next=prev;
+            prev=head;
+            head=next;
+        }
+        return prev;
     }
 }
