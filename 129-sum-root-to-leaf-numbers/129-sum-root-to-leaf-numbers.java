6d020050f305e9d863563a179f6e865b1d38f6b9
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-08-31 15:53:11 +0200

    Time: 0 ms (100.00%), Space: 39.8 MB (94.33%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.java
@@ -15,20 +15,34 @@
  */
 class Solution {
     public int sumNumbers(TreeNode root) {
-        List<Integer> paths=new ArrayList<>();
-        traverse(root,paths,0);
-        int res=0;
-        for(int n:paths) res+=n;
-        return res;
+        // List<Integer> paths=new ArrayList<>();
+        // traverse(root,paths,0);
+        // int res=0;
+        // for(int n:paths) res+=n;
+        return findRootToLeafPathNumbers(root,0);
     }
-    private void traverse(TreeNode root,List<Integer> paths,int path){
-        if(root==null) return;
-        path=(path*10)+root.val;
-        if(root.left==null&&root.right==null) paths.add(path);
-        else {
-            traverse(root.left,paths,path);
-            traverse(root.right,paths,path);
-        }
-        path/=10;
+    // private void traverse(TreeNode root,List<Integer> paths,int path){
+    //     if(root==null) return;
+    //     path=(path*10)+root.val;
+    //     if(root.left==null&&root.right==null) paths.add(path);
+    //     else {
+    //         traverse(root.left,paths,path);
+    //         traverse(root.right,paths,path);
+    //     }
+    //     path/=10;
+    // }
+     private static int findRootToLeafPathNumbers(TreeNode currentNode, int pathSum) {
+    if (currentNode == null)
+      return 0;
+
+    pathSum = 10 * pathSum + currentNode.val;
+
+    if (currentNode.left == null && currentNode.right == null) {
+      return pathSum;
     }
+
+    // traverse the left and the right sub-tree
+    return findRootToLeafPathNumbers(currentNode.left, pathSum) +
+           findRootToLeafPathNumbers(currentNode.right, pathSum);
+  }
 }
