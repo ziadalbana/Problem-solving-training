79ce1277c71740bec6a62c747566440b1bd7fb30
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-08-31 15:18:11 +0200

    Time: 1 ms (100.00%), Space: 43.3 MB (54.08%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.java
+++ 257-binary-tree-paths/257-binary-tree-paths.java
@@ -16,23 +16,20 @@
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> allPaths=new ArrayList<>();
-         traverse(root,allPaths,new ArrayList<>());
+         traverse(root,allPaths,new StringBuilder());
         return allPaths;
     }
-    private void traverse(TreeNode root,List<String> allPaths,List<Integer> path){
+    private void traverse(TreeNode root,List<String> allPaths,StringBuilder path){
         if(root==null) return ;
-        path.add(root.val);
+        int length=path.length();
         if(root.left==null&&root.right==null) {
-            String handler="";
-            for(int i=0;i<path.size();i++){
-                if(i<path.size()-1) handler+=path.get(i)+"->";
-                else handler+=path.get(i);
-            }
-            allPaths.add(handler);
+            path.append(root.val);
+            allPaths.add(path.toString());
         }else {
+            path.append(root.val).append("->");
             traverse(root.left,allPaths,path);
             traverse(root.right,allPaths,path);
         }
-        path.remove(path.size()-1);
+        path.setLength(length);
     }
 }
