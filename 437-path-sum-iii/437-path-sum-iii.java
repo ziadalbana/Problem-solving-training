0dcef711c7e337ec101436294980f021f84bd71a
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-09-01 11:45:56 +0200

    Time: 3 ms (99.76%), Space: 41.8 MB (97.29%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -14,22 +14,43 @@
  * }
  */
 class Solution {
-    public int pathSum(TreeNode root, int targetSum) {
+//     public int pathSum(TreeNode root, int targetSum) {
     
-    return traverse(root,targetSum,new ArrayList<>());
-  }
-  private static int traverse(TreeNode root, int S,List<Integer> path){
-   if(root==null) return 0;
-   int pathsCount=0;
-    long sum=0;
-   path.add(root.val);
-   for(int i=path.size()-1;i>=0;i--){
-     sum+=path.get(i);
-     if(sum==S) pathsCount++;
-   }
-   pathsCount+=traverse(root.left,S,path);
-   pathsCount+=traverse(root.right,S,path);
-   path.remove(path.size()-1);
-   return pathsCount;
-  }
+//     return traverse(root,targetSum,new ArrayList<>());
+//   }
+//   private static int traverse(TreeNode root, int S,List<Integer> path){
+//    if(root==null) return 0;
+//    int pathsCount=0;
+//    long sum=0;
+//    path.add(root.val);
+//    for(int i=path.size()-1;i>=0;i--){
+//      sum+=path.get(i);
+//      if(sum==S) pathsCount++;
+//    }
+//    pathsCount+=traverse(root.left,S,path);
+//    pathsCount+=traverse(root.right,S,path);
+//    path.remove(path.size()-1);
+//    return pathsCount;
+//   }
+     public int pathSum(TreeNode root, int targetSum) {
+         long n=0;
+         return traverse( root,targetSum,new HashMap<>(),n);
+     }
+    private int  traverse(TreeNode root, int sum,HashMap<Long,Integer> map,long pathSum){
+        if(root==null) return 0;
+        int pathCount=0;
+        pathSum+=root.val;
+        
+        if(pathSum==sum) pathCount++;
+        
+        pathCount+=map.getOrDefault((pathSum-sum),0);
+        map.put(pathSum,map.getOrDefault(pathSum,0)+1);
+        
+        pathCount+=traverse(root.left,sum,map,pathSum);
+        pathCount+=traverse(root.right,sum,map,pathSum);
+        
+        map.put(pathSum,map.getOrDefault(pathSum,1)-1);
+        return pathCount; 
+        
+    }
 }
