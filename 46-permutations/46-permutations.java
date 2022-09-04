73b91ca410583166367838fba2cbb8ee41068858
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-09-04 15:19:11 +0200

    Time: 2 ms (76.85%), Space: 45 MB (32.20%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,34 +1,17 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
     List<List<Integer>> result = new ArrayList<>();
-    Queue<List<Integer>> queue=new LinkedList<>();
-    queue.add(new ArrayList<>());
-    for(int num:nums){
-       int n=queue.size();
-       for(int i=0;i<n;i++){
-        List<Integer> oldPermutions=queue.poll();
-        for(int j=0;j<=oldPermutions.size();j++){
-           List<Integer> newPermutions=new ArrayList<>(oldPermutions);
-           newPermutions.add(j,num);
-           if(newPermutions.size()==nums.length){
-             result.add(newPermutions);
-           }else {
-             queue.add(newPermutions);
-           }
-        }
-       }
-    }
+    perm(nums,result,new ArrayList<>(),0);    
     return result;
 }
-    // private void perm(int[] nums, List<List<Integer>> result,List<Integer> temp){
-    //     if(temp.size()==nums.length)  result.add(new ArrayList<>(temp));
-    //     else{
-    //         for(int i=0;i<nums.length;i++){
-    //             if(temp.contains(nums[i])) continue;
-    //             temp.add(nums[i]);
-    //             perm(nums,result,temp);
-    //             temp.remove(temp.size()-1);
-    //         }
-    //     }
-    // }
+    private void perm(int[] nums, List<List<Integer>> result,List<Integer> temp,int index){
+        if(temp.size()==nums.length)  result.add(new ArrayList<>(temp));
+        else{
+            for(int i=0;i<=temp.size();i++){
+                List<Integer> currTemp = new ArrayList<>(temp);
+                currTemp.add(i,nums[index]);
+                perm(nums,result,currTemp,index+1);
+            }
+        }
+    }
 }
