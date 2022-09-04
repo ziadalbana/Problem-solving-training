8d4736250a71d512a3a0c600411a8b355c8774d6
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-09-04 15:52:21 +0200

    Time: 4 ms (76.66%), Space: 42.5 MB (99.79%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.java
+++ 784-letter-case-permutation/784-letter-case-permutation.java
@@ -1,5 +1,6 @@
 class Solution {
-    public List<String> letterCasePermutation(String s) {
+//    version 1 
+/*    public List<String> letterCasePermutation(String s) {
         List<String> result=new ArrayList();
         construct(result,s.toLowerCase(),0,"");
         return result;
@@ -17,5 +18,22 @@ class Solution {
              construct(result,s,i+1,temp+""+ss);
         }
         
+     }
+ */
+     public List<String> letterCasePermutation(String str){
+    List<String> permutations = new ArrayList<>();
+    // TODO: Write your code here
+    permutations.add(str);
+    for(int i=0;i<str.length();i++){
+      int n=permutations.size();
+      if(!Character.isLetter(str.charAt(i))) continue;
+      for(int j=0;j<n;j++){
+        char[] temp=permutations.get(j).toCharArray();
+        if(Character.isUpperCase(temp[i])) temp[i]=Character.toLowerCase(temp[i]);
+        else  temp[i]=Character.toUpperCase(temp[i]);
+        permutations.add(new String(temp));
+      }
     }
+    return permutations;
+     }
 }
