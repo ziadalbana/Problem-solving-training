82e3c3a9c914b3ff938231ad6b847c7aadc535f2
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-09-04 12:41:57 +0200

    Time: 1 ms (89.76%), Space: 43.1 MB (69.17%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,22 +1,16 @@
 class Solution {
    public List<List<Integer>> subsets(int[] nums) {
-        // List<List<Integer>> res=new ArrayList<>();
-        // traverse(nums,res,new ArrayList<>(),0);
-        // return res;
-      List<List<Integer>> subsets = new ArrayList<>();
-    // start by adding the empty subset
-    subsets.add(new ArrayList<>());
-    for (int currentNumber : nums) {
-      // we will take all existing subsets and insert the current number in them to create new subsets
-      int n = subsets.size();
-      for (int i = 0; i < n; i++) {
-        // create a new subset from the existing subset and insert the current element to it
-        List<Integer> set = new ArrayList<>(subsets.get(i));
-        set.add(currentNumber);
-        subsets.add(set);
+      List<List<Integer>> res=new ArrayList<>();
+      res.add(new ArrayList<>());
+      for(int num:nums){
+          int n=res.size();
+          for(int i=0;i<n;i++){
+              List<Integer> subset=new ArrayList<>(res.get(i));
+              subset.add(num);
+              res.add(subset);
+          }
       }
-    }
-    return subsets;
+      return res;
     }
     private void traverse(int[] graph,List<List<Integer>>  res,ArrayList<Integer> temp,int to){
         res.add(new ArrayList<>(temp));
