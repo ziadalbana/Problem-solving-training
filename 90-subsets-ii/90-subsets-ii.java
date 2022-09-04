7ead56e0a2af86eb3c8fecc4628cb784ca432446
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-09-04 13:30:49 +0200

    Time: 2 ms (86.58%), Space: 44.2 MB (42.60%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,9 +1,24 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> res=new ArrayList<>();
-        traverse(nums,res,new ArrayList<>(),0);
-        return res;
+        // Arrays.sort(nums);
+        // List<List<Integer>> res=new ArrayList<>();
+        // traverse(nums,res,new ArrayList<>(),0);
+        // return res;
+    List<List<Integer>> subsets = new ArrayList<>();
+    Arrays.sort(nums);
+    subsets.add(new ArrayList<>());
+    int end=0;
+    for(int i=0;i<nums.length;i++){
+      int start=0;
+      if(i>0&&nums[i]==nums[i-1]) start=end;
+      end =subsets.size();
+      for(int j=start;j<end;j++){
+        List<Integer> subset = new ArrayList<>(subsets.get(j));
+        subset.add(nums[i]);
+        subsets.add(subset);
+      }
+    }   
+    return subsets;
     }
     private void traverse(int[] graph,List<List<Integer>>  res,ArrayList<Integer> temp,int to){
         res.add(new ArrayList<>(temp));
