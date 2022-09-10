446f6648fae384c1d883b1bb79f778e511a48617
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-09-10 14:15:37 +0200

    Time: 0 ms (100.00%), Space: 41.8 MB (92.03%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,24 +1,42 @@
 class Solution {
-  public int search(int[] nums, int target) {
-      int l=0;int r=nums.length-1;
-      int result=-1;
-      while(l<=r){
-        int mid =l+(r-l)/2;
-        if(nums[mid]==target){
-          return mid;
+     public int search(int[] arr, int target) {
+        int left=0,right=arr.length-1;
+        while (left<=right){
+            int mid=left+(right-left)/2;
+            if(arr[mid]==target) return mid;
+            if(arr[left]<=arr[mid]){
+              if(arr[left]<=target&&target<arr[mid]) right=mid-1;
+              else left=mid+1;
+            }
+            else{
+                if(arr[mid]<target&&target<=arr[right]) left=mid+1;
+                else right=mid-1;
+            }
         }
-        if(nums[l]<=nums[mid]){
-              if(nums[mid]>target&&target>=nums[l]) r=mid-1;
-              else l=mid+1;
-        }
-        if(nums[mid]<=nums[r]){
-            if(nums[mid]<target&&target<=nums[r]) l=mid+1;
-            else r=mid-1;
-        }
-      }
-  return -1;
+     return -1;
+     }
+    //version 2
+//   public int search(int[] nums, int target) {
+//       int l=0;int r=nums.length-1;
+//       int result=-1;
+//       while(l<=r){
+//         int mid =l+(r-l)/2;
+//         if(nums[mid]==target){
+//           return mid;
+//         }
+//         if(nums[l]<=nums[mid]){
+//               if(nums[mid]>target&&target>=nums[l]) r=mid-1;
+//               else l=mid+1;
+//         }
+//         if(nums[mid]<=nums[r]){
+//             if(nums[mid]<target&&target<=nums[r]) l=mid+1;
+//             else r=mid-1;
+//         }
+//       }
+//   return -1;
   
-  }
+//   }
+    //version 3
     //  public int search(int[] nums, int target) {
     //    int l=0;int r= nums.length-1;
     //    int mid=(r-l)/2;
