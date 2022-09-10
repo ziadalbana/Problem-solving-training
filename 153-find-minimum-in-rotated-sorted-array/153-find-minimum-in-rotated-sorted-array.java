4a413993c9257d57c3e556a6d4ed930d42e19065
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-09-10 16:02:25 +0200

    Time: 1 ms (45.24%), Space: 43.2 MB (6.78%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -1,13 +1,21 @@
 class Solution {
-    public int findMin(int[] nums) {
-        int l=0;int r=nums.length-1;
-        int min=Integer.MAX_VALUE;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[l]<=nums[mid]&&nums[mid]<=nums[r]) return nums[l];
-            else if(nums[l]<=nums[mid]&&nums[mid]>=nums[r]) l=mid+1;
-            else if(nums[mid]<=nums[r]&&nums[l]>nums[mid]) r=mid;
+    public int findMin(int[] arr) {
+        int left=0,right=arr.length-1;
+        while (left<right){
+            int mid=left+(right-left)/2;
+            if(arr[left]<=arr[mid]&&arr[mid]<=arr[right]) return arr[left];
+            else if(arr[left]>arr[mid]&&arr[mid]<arr[right]) right=mid;
+            else left=mid+1;
         }
-        return -1;
+        return arr[left];
+        // int l=0;int r=nums.length-1;
+        // int min=Integer.MAX_VALUE;
+        // while(l<=r){
+        //     int mid=l+(r-l)/2;
+        //     if(nums[l]<=nums[mid]&&nums[mid]<=nums[r]) return nums[l];
+        //     else if(nums[l]<=nums[mid]&&nums[mid]>=nums[r]) l=mid+1;
+        //     else if(nums[mid]<=nums[r]&&nums[l]>nums[mid]) r=mid;
+        // }
+        // return -1;
     }
 }
