e2e17436ca8350479c5ef7e3617322c7aa4eca5a
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-09-10 22:10:43 +0200

    Time: 2 ms (58.69%), Space: 44.9 MB (63.76%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.java
+++ 260-single-number-iii/260-single-number-iii.java
@@ -1,19 +1,13 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-         int n1n2 = 0;
-        for (int num : nums) {
-            n1n2 ^= num;
-        }
-
-        // get the rightmost bit that is '1'
-        int rightmostSetBit = n1n2&-n1n2;
-        int num1 = 0, num2 = 0;
-        for (int num : nums) {
-            if ((num & rightmostSetBit) != 0) // the bit is set
-                num1 ^= num;
-            else // the bit is not set
-                num2 ^= num;
-        }
-        return new int[] { num1, num2 };
+      int xy=0;
+        int num1=0,num2=0;
+      for(int n:nums) xy^=n;
+      int rightMostSetBit=xy&-xy;
+      for(int n:nums){
+          if((rightMostSetBit&n)==0) num1^=n;
+          else num2^=n;
+      }
+    return new int[]{num1,num2};
     }
 }
