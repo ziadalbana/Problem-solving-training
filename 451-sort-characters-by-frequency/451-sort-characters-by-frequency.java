ebdbf1c477d99ef84ffaf5f5140d4011da8750b7
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-09-12 23:34:52 +0200

    Time: 41 ms (30.74%), Space: 54.4 MB (34.34%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -5,11 +5,11 @@ class Solution {
     for(char c:s.toCharArray()) freqMap.put(c,freqMap.getOrDefault(c,0)+1);
     PriorityQueue<Map.Entry<Character,Integer>> maxHeap=new PriorityQueue<>((a1,a2)->a2.getValue()-a1.getValue());
     for(Map.Entry<Character,Integer> entry:freqMap.entrySet()) maxHeap.add(entry);
-    String res="";
+    StringBuilder res=new StringBuilder();
     while(!maxHeap.isEmpty()){
       Map.Entry<Character,Integer> entry =maxHeap.poll();
-      for(int i=0;i<entry.getValue();i++) res+=entry.getKey();
+      for(int i=0;i<entry.getValue();i++) res.append(entry.getKey());
     }
-    return res;
+    return res.toString();
     }
 }
