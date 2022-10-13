69e10376cf023653361472a1098e95d5e72529e6
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-10-13 23:03:07 +0200

    Time: 4 ms (83.23%), Space: 46.2 MB (56.96%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
        int[] map=new int[26];
-        for(char c:magazine.toCharArray()) map[c-'a']+=1;
-        for(char c:ransomNote.toCharArray()) map[c-'a']-=1;
+        for(char c:magazine.toCharArray()) map[c-97]++;
+        for(char c:ransomNote.toCharArray()) map[c-97]--;
         for(int i=0;i<26;i++) {
             if(map[i]<0) return false;
         }
