cff21a464cec49929fb34b9466798bcf71c0282a
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-10-17 12:27:38 +0200

    Time: 1 ms (97.52%), Space: 44.3 MB (49.62%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,9 +1,24 @@
 class Solution {
+    //version 1
+    // public int maxProfit(int[] prices) {
+    //     int profit=0;
+    //     for(int i=1;i<prices.length;i++){
+    //         profit+=Integer.max(prices[i]-prices[i-1],0);
+    //     }
+    //     return profit;
+    // }
+    // version 2
     public int maxProfit(int[] prices) {
-        int profit=0;
-        for(int i=1;i<prices.length;i++){
-            profit+=Integer.max(prices[i]-prices[i-1],0);
+        int i=0,profit=0;
+        int buy,sell;
+        while(i<prices.length-1){
+            while(i<prices.length-1&&prices[i]>=prices[i+1]) i++;
+            buy=prices[i];
+            while(i<prices.length-1&&prices[i]<=prices[i+1]) i++;
+            sell=prices[i];
+            profit+=sell-buy;
         }
         return profit;
+        
     }
 }
