0b0b7869cf9328aa088413cef32732392c483bd5
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-10-23 01:48:25 +0200

    Time: 15 ms (52.42%), Space: 45.9 MB (38.52%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -0,0 +1,56 @@
+import java.util.Hashtable;
+class Solution {
+ // public int lengthOfLongestSubstring(String s) {
+ //    //     int maxCount=Integer.MIN_VALUE;
+ //    //     int count=0;
+ //    //     int start=0;
+ //    //     Hashtable<Character,Integer> indexs=new Hashtable<>();
+ //    //     if(s.length()==1) return 1;
+ //    //     if(s.length()==0) return 0;
+ //    //     for (int i = 0; i < s.length(); i++) {
+ //    //         char ss=s.charAt(i);
+ //    //         if(indexs.containsKey(ss)&&indexs.get(ss)>=start){
+ //    //             maxCount=Integer.max(maxCount,count);
+ //    //             count=i-(indexs.get(ss)+1);
+ //    //             start=indexs.get(ss)+1;
+ //    //             indexs.put(ss,i);
+ //    //         }else{
+ //    //             indexs.put(s.charAt(i),i);
+ //    //         }
+ //    //         count++;
+ //    //     }
+ //    //     maxCount=Integer.max(maxCount,count);
+ //    //     return maxCount;
+ //    // }
+public int lengthOfLongestSubstring(String s) {
+        // // TODO: Write your code here
+        // if(s.length()==0) return 0;
+        // HashSet<Character> map=new HashSet<>();
+        // int windowStart=0;
+        // int windowEnd;
+        // int maxLength=-1;
+        // for(windowEnd=0;windowEnd<s.length();windowEnd++){
+        //     char c=s.charAt(windowEnd);
+        //     while(map.contains(c)) {
+        //         char ss=s.charAt(windowStart);
+        //         map.remove(ss);
+        //         windowStart++;
+        //     }
+        //     maxLength=Integer.max(maxLength,windowEnd-windowStart+1);
+        //     map.add(c);
+        // }
+        // return maxLength ;
+    // TODO: Write your code here
+    int windowStart=0;
+    int maxLength=-1;
+    HashSet<Character> freq=new HashSet<>();
+    if(s.length()==0) return 0;
+    for(int windowEnd=0;windowEnd<s.length();windowEnd++){
+       char c=s.charAt(windowEnd);
+       while(freq.contains(c)) freq.remove(s.charAt(windowStart++));
+       maxLength=Integer.max(maxLength,windowEnd-windowStart+1);
+       freq.add(c);
+    }
+    return maxLength;
+    }
+}
