f019e1ac2b7655643f256c02ed8dc289b392ee06
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-10-27 14:32:34 +0200

    Time: 3 ms (77.29%), Space: 42.2 MB (38.27%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isValid(String s) {
+       Stack<Character> stack=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='(') stack.add(')');
+            else if(c=='[') stack.add(']');
+            else if(c=='{') stack.add('}');
+            else if(stack.isEmpty()||c!=stack.pop()) return false;
+        }
+        return stack.isEmpty();
+    }
+}
