45bc04408be14ccb617d1840e639d64c6e1f6586
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-10-29 23:59:44 +0200

    Time: 0 ms (100.00%), Space: 43.4 MB (28.13%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -20,28 +20,4 @@ class Solution {
         int right=maxDepth(root.right);
         return Integer.max(left,right)+1;
     }
-    // public int maxDepth(TreeNode root) {
-    // if(root==null) return 0;
-    // Queue<TreeNode> queue=new LinkedList<>();
-    // queue.add(root);
-    // int len=0;
-    // while(!queue.isEmpty()){
-    //   int levelSize=queue.size();
-    //   for(int i=0;i<levelSize;i++){
-    //    TreeNode temp=queue.poll();
-    //    if(temp.left!=null) queue.offer(temp.left);
-    //    if(temp.right!=null) queue.offer(temp.right);
-    //   }
-    //   len++;
-    // }
-    // return len;
-    // }
-//     public int maxDepth(TreeNode root) {
-//         if(root==null) return 0;
-//         int leftLength=maxDepth(root.left);
-//         int rightLength=maxDepth(root.right);
-//         return Integer.max(leftLength,rightLength)+1;
-        
-        
-//     }
 }
