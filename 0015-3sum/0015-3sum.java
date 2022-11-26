31efdc06b982885e8355df09f3ff521349dc54d8
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-11-26 15:26:34 +0200

    Time: 25 ms (93.42%), Space: 58 MB (78.37%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,23 +1,24 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        for(int i=0;i<nums.length-2;i++){
-            if(i>0&&nums[i-1]==nums[i]) continue;
-            findPair(res,nums,-nums[i],i+1);
+        List<List<Integer>> res=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(i!=0&&nums[i-1]==nums[i]) continue;
+            findPair(res,nums,i);
         }
         return res;
     }
-    private void findPair(List<List<Integer>> res,int[] nums,int target,int start ){
-        int left=start,right=nums.length-1;
+    private void findPair(List<List<Integer>> res,int[] nums,int idx){
+        int target=nums[idx];
+        int left=idx+1,right=nums.length-1;
         while(left<right){
-            int sum=nums[left]+nums[right];
-            if(target==sum) {
-                res.add(Arrays.asList(-target,nums[left++],nums[right--]));
+            int sum=target+nums[left]+nums[right];
+            if(sum==0){
+                res.add(Arrays.asList(target,nums[left++],nums[right--]));
                 while(left<right&&nums[left]==nums[left-1]) left++;
-                while(left<right&&nums[right]==nums[right+1]) right--;          
+                while(left<right&&nums[right]==nums[right+1]) right--;
             }
-            else if(sum>target) right--;
+            else if(sum>0) right--;
             else left++;
         }
     }
