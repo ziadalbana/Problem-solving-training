3ae115bdd28b6bdc6697824c658d212e7f7bb027
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2022-12-01 17:49:36 +0200

    Time: 101 ms (27.48%), Space: 48.3 MB (35.36%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,32 +1,26 @@
-public class Solution {
-    public  List<Integer> findAnagrams(String str, String pattern) {
-    if(pattern.length()>str.length()) return new ArrayList<Integer>();
-    List<Integer> resultIndices = new ArrayList<Integer>();
-    // TODO: Write your code here
-    HashMap<Character,Integer> map=new HashMap<>();
-    for(char c:pattern.toCharArray()) map.put(c,map.getOrDefault(c,0)+1);
-    int windowStart=0;
-    int matched=0;
-    for(int windowEnd=0;windowEnd<str.length();windowEnd++){
-       char r=str.charAt(windowEnd);
-       if(map.containsKey(r)){
-         map.put(r,map.get(r)-1);
-         if(map.get(r)==0) matched++;
-       }
-       if(matched==map.size()){
-         resultIndices.add(windowStart);
-       }
-       if(windowEnd>=pattern.length()-1){
-         char l=str.charAt(windowStart++);
-         if(map.containsKey(l)){
-            if(map.get(l)==0) matched--;
-            map.put(l,map.get(l)+1);
-         }
-       }
-
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        ArrayList<Integer> idx=new ArrayList<>();
+        if(s.length()<p.length()) return idx;
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char c:p.toCharArray())map.put(c,map.getOrDefault(c,0)+1);
+        int windowStart=0;
+        int matched=0;
+        for(int windowEnd=0;windowEnd<s.length();windowEnd++){
+            char c=s.charAt(windowEnd);
+            if(map.containsKey(c)){
+                map.put(c,map.get(c)-1);
+                if(map.get(c)==0) matched++;
+            }
+            if(windowEnd>=p.length()-1){
+                if(matched==map.size()) idx.add(windowStart);
+                char j=s.charAt(windowStart++);
+                if(map.containsKey(j)){
+                    if(map.get(j)==0) matched--;
+                    map.put(j,map.get(j)+1);
+                }
+            }
+        }
+        return idx;
     }
-
-
-    return resultIndices;
-  }
 }
