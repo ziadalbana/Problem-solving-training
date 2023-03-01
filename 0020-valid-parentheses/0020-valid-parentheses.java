a1a85f71fb683823797e4f31ef132dc0b2c5c8fa
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2023-03-01 15:44:29 +0200

    Time: 2 ms (59.02%), Space: 40.6 MB (35.24%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean isValid(String s) {
-       Stack<Character> stack=new Stack<>();
+        Stack<Character> stack=new Stack<>();
         for(char c:s.toCharArray()){
             if(c=='(') stack.add(')');
             else if(c=='[') stack.add(']');
-            else if(c=='{') stack.add('}');
+            else if (c=='{') stack.add('}');
             else if(stack.isEmpty()||c!=stack.pop()) return false;
         }
         return stack.isEmpty();
