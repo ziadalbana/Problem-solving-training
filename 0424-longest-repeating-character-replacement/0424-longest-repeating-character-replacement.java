9bfa7d18ccb2799d0b6b023ca44692e7a34eae52
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2023-03-14 03:04:39 +0200

    Time: 30 ms (21.26%), Space: 42.8 MB (16.44%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,19 +1,17 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-    // TODO: Write your code here
-    int windowStart=0;
-    int maxLength=0,maxRepeating=0;
-    HashMap<Character,Integer> map=new HashMap<>();
-    for(int windowEnd=0;windowEnd<s.length();windowEnd++){
-         char c=s.charAt(windowEnd);
-         map.put(c,map.getOrDefault(c,0)+1);
-         maxRepeating=Integer.max(maxRepeating,map.get(c));
-         if(windowEnd-windowStart-maxRepeating+1>k){
-           char leftChar=s.charAt(windowStart++);
-           map.put(leftChar,map.get(leftChar)-1);
-         }
-         maxLength=Integer.max(maxLength,windowEnd-windowStart+1);
+        HashMap<Character,Integer> map=new HashMap<>();
+        int windowStart=0,windowEnd=0,repeated=0,maxLength=0;
+        for(windowEnd=0;windowEnd<s.length();windowEnd++){
+            char c=s.charAt(windowEnd);
+            map.put(c,map.getOrDefault(c,0)+1);
+            repeated=Integer.max(map.get(c),repeated);
+            if(windowEnd-windowStart+1-repeated>k){
+                char l=s.charAt(windowStart++);
+                map.put(l,map.get(l)-1);
+            }
+            maxLength=Integer.max(windowEnd-windowStart+1,maxLength);
+        }
+        return maxLength;
     }
-    return maxLength;
-  }
 }
