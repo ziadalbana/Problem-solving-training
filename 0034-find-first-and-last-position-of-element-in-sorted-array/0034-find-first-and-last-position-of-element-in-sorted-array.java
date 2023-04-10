a020ab0ec92b23be32c8c53d2828c34e2ca5b70b
Ziad Albana <58531158+ziadalbana@users.noreply.github.com>
2023-04-10 17:44:57 +0200

    Time: 0 ms (100.00%), Space: 46.2 MB (18.03%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,34 +1,34 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int lower= findLower(nums,target);
-        int upper=findUpper(nums,target);
-         return new int[]{lower,upper};
+        int l=searchLeft(nums,target);
+        int r=searchRight(nums,target);
+        return new int[]{l,r};
     }
-    private int findLower(int[] nums,int target){
+    private int searchLeft(int[] nums,int target){
         int l=0,r=nums.length-1;
         int result=-1;
         while(l<=r){
             int mid=l+(r-l)/2;
-            if(target==nums[mid]){
+            if(nums[mid]==target){
                 result=mid;
                 r--;
-            }else if(nums[mid]>target) r=mid-1;
-            else l=mid+1;
+            }else if(nums[mid]<target) l=mid+1;
+            else r=mid-1;
         }
-        return result;
+        return result ;
     }
-    private int findUpper(int[] nums,int target){
-        int l=0,r=nums.length-1;
-        int result=-1;
+    private int searchRight(int[] nums,int target){
+          int l=0,r=nums.length-1;
+          int result=-1;
         while(l<=r){
             int mid=l+(r-l)/2;
-            if(target==nums[mid]){
+            if(nums[mid]==target){
                 result=mid;
                 l++;
-            }else if(nums[mid]>target) r=mid-1;
-            else l=mid+1;
+            }
+            else if(nums[mid]<target) l=mid+1;
+            else r=mid-1;
         }
-        return result;
+        return result ;
     }
-    
 }
