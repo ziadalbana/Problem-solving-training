256992f5c8d2093364ca7b85dd02c3473cd9f3bc
Ziad Albana <zeaid.allbana@gmail.com>
2024-06-17 15:25:09 +0200

    Time: 1 ms (78.29%), Space: 45.3 MB (84.78%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,28 +1,31 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int n=matrix.length,m=matrix[0].length;
-        boolean[][] changed =new boolean[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==0&&!changed[i][j])
-                    setZero(i,j,matrix,changed);
-            }
-        }  
+      int n=matrix.length,m=matrix[0].length;
+      boolean[][] visited=new boolean[n][m];
+      for(int i=0;i<n;i++){
+          for(int j=0;j<m;j++){
+              if(matrix[i][j]==0&&!visited[i][j]){
+                  visited[i][j]=true;
+                  setRowAndCol(matrix,visited,i,j);
+              }
+          }
+      }
     }
-    private void setZero(int x,int y,int[][] matrix,boolean[][] changed){
+    
+    private void setRowAndCol(int[][] matrix,boolean[][] visited,int x,int y){
         int n=matrix.length,m=matrix[0].length;
-        changed[x][y]=true;
-        // row iteration
+        //row iteration
         for(int i=0;i<m;i++){
-            if(!changed[x][i]&&matrix[x][i]!=0) {
-                matrix[x][i]=0;
-                changed[x][i]=true;
+            if(matrix[x][i]!=0&&!visited[x][i]){
+              visited[x][i]=true;
+              matrix[x][i]=0;
             }
         }
+        // col Iteration
         for(int i=0;i<n;i++){
-            if(!changed[i][y]&&matrix[i][y]!=0) {
-                matrix[i][y]=0;
-                changed[i][y]=true;
+            if(matrix[i][y]!=0&&!visited[i][y]){
+              visited[i][y]=true;
+              matrix[i][y]=0;
             }
         }
     }
