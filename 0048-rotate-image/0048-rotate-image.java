daa0efdb602db40312392e9a6f8102314ed29553
Ziad Albana <zeaid.allbana@gmail.com>
2024-06-30 19:38:46 +0300

    Time: 0 ms (100.00%), Space: 41.7 MB (89.76%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,17 +1,22 @@
 class Solution {
-public void rotate(int[][] matrix) {
+    public void rotate(int[][] matrix) {
         int n=matrix.length;
-        int m=matrix[0].length;
-        int levels=(n+1)/2;
-        for(int i=0;i<levels;i++){
-            int cols=m-(2*i)-1;
-            for(int j=i;j<cols+i;j++){
+        int moves=n/2;
+        int size=n;
+        for(int i=0;i<moves;i++){
+            for(int j=i;j<size-1;j++){
+                 // temp first in iteration corner top
                 int temp=matrix[i][j];
-                matrix[i][j]=matrix[n-j-1][i];
-                matrix[n-j-1][i]=matrix[n-i-1][m-j-1];
-                matrix[n-i-1][m-j-1]=matrix[j][m-i-1];
-                matrix[j][m-i-1]=temp;
+                // swap cornerdownleft with corner up left
+                matrix[i][j]=matrix[n-1-j][i];
+                // swap corener down right with corener down left
+                matrix[n-1-j][i]=matrix[n-1-i][n-1-j];
+                // swap corener up right with corener down right
+                matrix[n-1-i][n-1-j]=matrix[j][n-1-i];
+                // swap corener up left with corener up right
+                matrix[j][n-1-i]=temp;
             }
+            size--;
         }
     }
 }
